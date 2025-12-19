76e176a5f0530573d05ace187df40235fac193a1
YakiQuiso <guieamorim@protonmail.com>
2025-12-19 15:54:08 -0300

    simple version with all features

---
+++ app.js
@@ -0,0 +1,259 @@
+const translations = {
+  en: {
+
+    titleH1: "Yaki Quiso's<br>Portfolio",
+    programmingNav: "Programming",
+    musicNav: "Music",
+    videoEditingNav: "Video Editing",
+    art2dNav: "2D art",
+    aboutMeNav: "About Me",
+    linksNav: "Links",
+
+
+    programmingH2: "Programming Portfolio",
+    programmingExperienceH3: "I have experience on:",
+    programmingExperienceLi1: "HTML",
+    programmingExperienceLi2: "CSS",
+    programmingExperienceLi3: "JS",
+    programmingExperienceLi4: "Git",
+
+    programmingSoftwareH3: "On the following software:",
+    programmingSoftwareLi1: "Visual Studio Code",
+    programmingSoftwareLi2: "Github",
+
+    programmingProjectsH3: "Some projects and portfolio:",
+    programmingProjectsLi1: "This website",
+    programmingProjectsLi2: "Analysis and Systems Development degree (finished on July of 2026)",
+    programmingProjectsLi3: "Responsive Web Design by freeCodeCamp",
+    
+    programmingOngoingH3: "Ongoing:",
+    programmingOngoingLi1: "JavaScript by freeCodeCamp",
+    programmingOngoingLi2: "Front End Development Libraries by freeCodeCamp",
+    programmingOngoingLi3: "Python by freeCodeCamp",
+    programmingOngoingLi4: "Relational Databases by freeCodeCamp",
+    programmingOngoingLi5: "Back End Development and APIs by freeCodeCamp",
+    programmingOngoingLi6: "Certified Full Stack Developer Curriculum by freeCodeCamp",
+
+
+    musicH2: "Music Portfolio",
+
+    musicExperienceH3: "I have experience on:",
+    musicExperienceLi1: "Rock",
+    musicExperienceLi2: "Metal",
+    musicExperienceLi3: "Punk",
+    musicExperienceLi4: "Symphonic Metal",
+    musicExperienceLi5: "Ambient",
+
+    musicSoftwareH3: "On the following software:",
+    musicSoftwareLi1: "Ardour",
+    musicSoftwareLi2: "Fl Studio",
+    musicSoftwareLi3: "Calf plugins",
+    musicSoftwareLi4: "Kontakt",
+    musicSoftwareLi5: "Surge XT",
+    musicSoftwareLi6: "Synthesizer V",
+
+    musicInstrumentsH3: "With the following instruments:",
+    musicInstrumentsLi1: "Piano",
+    musicInstrumentsLi2: "Violin",
+    musicInstrumentsLi3: "Electric Guitar",
+    musicInstrumentsLi4: "Drums",
+    musicInstrumentsLi5: "Bass",
+    musicInstrumentsLi6: "Clarinet",
+    musicInstrumentsLi7: "Tuba",
+    musicInstrumentsLi8: "Flute",
+    musicInstrumentsLi9: "Sax",
+    musicInstrumentsLi10: "Trumpet",
+    musicInstrumentsLi11: "Trombone",
+    musicInstrumentsLi12: "Guitar",
+    musicInstrumentsLi13: "Fluegelhorn",
+    musicInstrumentsLi14: "Cello",
+    musicInstrumentsLi15: "Synthesizer",
+
+    musicProjectsH3: "Some projects and portfolio:",
+    musicProjectsP1: "Epic Symphonic Metal with English SynthV Vocals",
+    musicProjectsP2: "Sinister Song with Incomprehensible Whispers Using SynthV Vocals",
+
+
+    videoEditingH2: "Still learning this!",
+
+
+    art2dH2: "Still learning this!",
+
+
+    aboutMeH2: "My Story:",
+    aboutMeP: `I'm Guilherme Ebersol and I'm known as Yaki Quiso on social media, I'm from 2005 borned on the South of Brazil, 
+      I quickly gained interest on computers and with time developed several hobbies such as music (since 2020) and programming (since 2022) 
+      Because of music I needed to create music videos so more people could see my creations which led me to create videos using Kdenlive (2024) 
+      and some time after using Davinci Resolve (2026), I also started to learn and practice digital 2D art (2026), I post all my music on
+      music platforms since March of 2025, at that point I had already +100 songs created now I'm reaching the 150 mark.`,
+  },
+
+  pt:  {
+
+    titleH1: "Portfolio de<br>Yaki Quiso",
+    programmingNav: "Programação",
+    musicNav: "Musica",
+    videoEditingNav: "Edição de Video",
+    art2dNav: "Arte 2D",
+    aboutMeNav: "Sobre Mim",
+    linksNav: "Links",
+
+
+    programmingH2: "Portfolio de Programação",
+    programmingExperienceH3: "Possuo experiencia em:",
+    programmingExperienceLi1: "HTML",
+    programmingExperienceLi2: "CSS",
+    programmingExperienceLi3: "JS",
+    programmingExperienceLi4: "Git",
+
+    programmingSoftwareH3: "Nos seguintes softwares:",
+    programmingSoftwareLi1: "Visual Studio Code",
+    programmingSoftwareLi2: "Github",
+
+    programmingProjectsH3: "Alguns projetos e portfolio:",
+    programmingProjectsLi1: "Esse website",
+    programmingProjectsLi2: "Graduação em analise e desenvolvimento de sistemas (finalizada em Julho de 2026)",
+    programmingProjectsLi3: "Design Web Responsivo por freeCodeCamp",
+    
+    programmingOngoingH3: "Em progresso:",
+    programmingOngoingLi1: "JavaScript por freeCodeCamp",
+    programmingOngoingLi2: "Bibliotecas de desenvolvimento Front End por freeCodeCamp",
+    programmingOngoingLi3: "Python por freeCodeCamp",
+    programmingOngoingLi4: "Banco de dados relacionais por freeCodeCamp",
+    programmingOngoingLi5: "APIs e desenvolvimento Back End por freeCodeCamp",
+    programmingOngoingLi6: "Certificado do curriculo de desenvolver Full Stack por freeCodeCamp",
+
+
+    musicH2: "Portfolio de Musica",
+
+    musicExperienceH3: "Possuo experiencia em:",
+    musicExperienceLi1: "Rock",
+    musicExperienceLi2: "Metal",
+    musicExperienceLi3: "Punk",
+    musicExperienceLi4: "Metal Sinfonico",
+    musicExperienceLi5: "Ambiente",
+
+    musicSoftwareH3: "Nos seguintes softwares:",
+    musicSoftwareLi1: "Ardour",
+    musicSoftwareLi2: "Fl Studio",
+    musicSoftwareLi3: "Calf plugins",
+    musicSoftwareLi4: "Kontakt",
+    musicSoftwareLi5: "Surge XT",
+    musicSoftwareLi6: "Synthesizer V",
+
+    musicInstrumentsH3: "Com os seguintes instrumentos:",
+    musicInstrumentsLi1: "Piano",
+    musicInstrumentsLi2: "Violino",
+    musicInstrumentsLi3: "Guitarra",
+    musicInstrumentsLi4: "Bateria",
+    musicInstrumentsLi5: "Baixo",
+    musicInstrumentsLi6: "Clarinete",
+    musicInstrumentsLi7: "Tuba",
+    musicInstrumentsLi8: "Flauta",
+    musicInstrumentsLi9: "Saxofone",
+    musicInstrumentsLi10: "Trombeta",
+    musicInstrumentsLi11: "Trombone",
+    musicInstrumentsLi12: "Violão",
+    musicInstrumentsLi13: "Fluegelhorn",
+    musicInstrumentsLi14: "Violoncelo",
+    musicInstrumentsLi15: "Sintetizador",
+
+    musicProjectsH3: "Alguns projetos e portfolio:",
+    musicProjectsP1: "Metal Sinfonico Epico com Vocais SynthV em Inglês",
+    musicProjectsP2: "Musica Sinistra com Sussurros Imcompreensiveis usando Vocals SynthV",
+
+
+    videoEditingH2: "Ainda estou aprendendo isso!",
+
+
+    art2dH2: "Ainda estou aprendendo isso!",
+
+
+    aboutMeH2: "Minha História:",
+    aboutMeP: `Eu me chamo Guilherme Ebersol e sou conhecido como Yaki Quiso nas redes sociais, 
+    Sou de 2005 nascido no sul do Brasil, Rapidamente ganhei interesse em computadores e 
+    com o tempo desenvolvi varios hobbies, como a musica (desde 2020) e programação (desde 2022), 
+    Por causa da musica precisei aprender a criar clipes musicais para mais pessoas 
+    assistirem minhas criações, o que me levou a criar videos usando Kdenlive (2024) e 
+    um tempo depois a usar Davinci Resolve (2026), tambem comecei a aprender e praticar arte 
+    digital 2D (2026), Publico todas minhas musicas em plataformas de musica desde março de 2025, 
+    naquele ponto ja havia criado +100 musicas, agora estou chegando na marca de 150.`,
+  }
+};
+
+function copyText() {
+  navigator.clipboard.writeText ("yakisoquiso@gmail.com");
+  document.getElementById('copy-gmail').innerHTML = "You copied my Gmail!";
+}
+
+function showSection(id) {
+  fadeActiveSection(() => {
+    document.querySelectorAll("main section").forEach(section => {
+      section.classList.remove("active");
+    });
+
+    const target = document.getElementById(id);
+    target.classList.add("active");
+  });
+}
+
+
+function fadeActiveSection(callback) {
+  const activeSection = document.querySelector("main section.active");
+
+  if (!activeSection) {
+    callback();
+    return;
+  }
+
+  activeSection.classList.add("fade-out");
+
+  setTimeout(() => {
+    callback();
+
+    const newActive = document.querySelector("main section.active");
+    newActive.classList.add("fade-in");
+
+    setTimeout(() => {
+      newActive.classList.remove("fade-in");
+    }, 200);
+  }, 200);
+}
+
+showSection("programming");
+
+function setLanguage(lang) {
+  fadeActiveSection(() => {
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+      const key = el.dataset.i18n;
+      el.innerHTML = translations[lang][key];
+  });
+
+    const title = document.querySelector("nav h1");
+    title.classList.remove("fade-in-left");
+
+    void title.offsetWidth;
+
+    title.classList.add("fade-in-left");
+
+    document
+      .querySelectorAll(".lang-btn")
+      .forEach(btn => btn.classList.remove("active"));
+
+    document
+      .querySelector(`.lang-btn[onclick="setLanguage('${lang}')"]`)
+      ?.classList.add("active");
+
+    localStorage.setItem("lang", lang)
+  });  
+}
+
+window.addEventListener("load", () => {
+  document.querySelector("nav h1").classList.add("fade-in-left");
+})
+
+const userLang =
+  localStorage.getItem("lang") ||
+  (navigator.language.startsWith("pt") ? "pt" : "en");
+
+setLanguage(userLang);
